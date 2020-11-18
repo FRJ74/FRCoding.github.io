867e838fff8f8621ce391bd5b70adb3174172401
FRJ74 <frankr74@gmail.com>
2020-11-18 01:16:42 -0600

    seachbox & slideoutMenu added functionality of searchbox & slideoutmenue wit breakpointsm

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ searchIcon.addEventListener('click',function() {
       searchBox.style.pointerEvents = 'auto'
   }
     });
+
+    menuIcon.addEventListener('click' , function() {
+        if (slideoutMenu.style.opacity == "1") {
+            slideoutMenu.style.opacity = '0';
+            slideoutMenu.style.pointerEvents = 'none';
+        } else {
+            slideoutMenu.style.opacity = '1';
+            slideoutMenu.style.pointerEvents = 'auto';
+        }
+    })
